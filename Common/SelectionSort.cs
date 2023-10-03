d032420c6254951ee9600223bb796236c75dc36b
Vitoria-Gasteizko Ingeniaritza Eskola UPV-EHU <ehu-vgie@hotmail.com>
2023-10-03 17:13:38 +0200

    Add project files.

---
+++ Common/SelectionSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class SelectionSort : ISortingAlgorithm
+    {
+        
+
+        public void Sort(int[] A)
+        {
+            //TODO #1: Implement SelectionSort
+            int n=A.Length;
+            for(int pivot = 0; pivot < n-1; pivot++)
+            {
+                for(int i = pivot + 1; i < n; i++)
+                {
+                    if (A[pivot] > A[i])
+                        Swap(A, pivot, i);
+                }
+            }
+            
+        }
+
+        private void Swap(int[] a, int pivot, int i)
+        {
+            int aux= a[pivot];
+            a[pivot]= a[i];
+            a[i]= aux;
+        }
+
+        public bool CheckIsCorrect()
+        {
+            int n = 10;
+            int[] A = Utils.CreateIntArray(n);
+
+            Console.WriteLine("1.1 Checking Sort()");
+            Sort(A);
+            if (!Utils.IsOrdered(A))
+            {
+                Console.WriteLine("FAILED");
+                return false;
+            }
+            Console.WriteLine("PASSED");
+            return true;
+        }
+    }
+}
